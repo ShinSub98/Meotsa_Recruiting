8042c927a584322219404fcb66c80c63fa03018b
ShinSub98 <wjdtjq26@naver.com>
2024-01-27 23:31:29 +0900

    feat: add delete view

--- apply/urls.py
+++ apply/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'notice'
 
 urlpatterns = [
-    path('', ApplyView.as_view())
+    path('', ApplyView.as_view()),
+    path('del/<int:pk>/', DelApplication.as_view()),
 ]
--- apply/views.py
+++ apply/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from .serializers import ApplicationSerializer
 from .models import Application
@@ -6,4 +6,7 @@ from .models import Application
 # Create your views here.
 class ApplyView(ListCreateAPIView):
     serializer_class = ApplicationSerializer
+    queryset = Application.objects.all()
+
+class DelApplication(DestroyAPIView):
     queryset = Application.objects.all()
