444b6cf28b173ff3bfcee0ab9e30b873ccb91cfa
ShinSub98 <wjdtjq26@naver.com>
2024-01-27 22:39:44 +0900

    chore: cors settings

--- recruiting/settings.py
+++ recruiting/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'apply',
 ]
 
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'recruiting.urls'
@@ -131,6 +133,12 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+"http://localhost:3000",
+"http://127.0.0.1:8000"]
+
+CORS_ALLOW_CREDENTIALS = True
 
 try:
     from .local_settings import *
